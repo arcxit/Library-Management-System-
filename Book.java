bca1e9d7ecd47beec69bbb558ef53bd51b3a933f
arcxit <120356456+arcxit@users.noreply.github.com>
2025-08-31 19:20:01 +0530

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,8 @@
+public class Book {
+    public int id;
+    public String title;
+    public String author;
+    public Book(int id, String title, String author){
+        this.id = id; this.title = title; this.author = author;
+    }
+}
