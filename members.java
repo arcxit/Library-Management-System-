13b79a45771c47e270f6bd9759155532798d9098
arcxit <120356456+arcxit@users.noreply.github.com>
2025-08-31 19:20:52 +0530

    Create members.java

---
+++ members.java
@@ -0,0 +1,8 @@
+public class Member {
+    public int id;
+    public String name;
+    public String email;
+    public Member(int id, String name, String email){
+        this.id = id; this.name = name; this.email = email;
+    }
+}
